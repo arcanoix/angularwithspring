4c900a8997566c00c8abb58dd926827701c64c68
Gustavo Herrera <gustavoh.2312@gmail.com>
2020-03-28 07:16:35 -0500

    creando clase cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Cliente} from './cliente';
 
 @Component({
   selector: 'app-clientes',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    {id: 1, nombre: 'gustavo', apellido:'herrera', email:'gus@gmail.com', createAt:'2017-11-20'},
+    {id: 1, nombre: 'dairy', apellido:'sanabria', email:'day@gmail.com', createAt:'2017-11-20'},
+  ];
 
   constructor() { }
 
