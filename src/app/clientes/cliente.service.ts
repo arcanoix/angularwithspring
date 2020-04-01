cd2d7687b4db5fd8c6ed46500836fdfe77fd0bfe
Gustavo Herrera <gustavoh.2312@gmail.com>
2020-04-01 06:40:48 -0500

    agregando eliminar

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -39,4 +39,8 @@ export class ClienteService {
       return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders});
     }
 
+    delete(id: number): Observable<Cliente> {
+      return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders})
+    }
+
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
 import {ClienteService} from './cliente.service';
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-clientes',
@@ -17,6 +18,32 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(
         clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+    swal({
+      title: 'Esta seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si!'
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swal(
+              'Eliminado!',
+              'El cliente a sido eliminado.',
+              'success'
+            )
+          }
+        )
+        
+      }
+    })
   }
 
 }
